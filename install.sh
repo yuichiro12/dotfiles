6c7934e461bd1f0215f0efbccd2a23bd1b766e52
yuichiro12 <talcnrhyme@gmail.com>
2022-10-24 18:33:33 +0900

    a

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 apt-get install fzf
-fisher install oh-my-fish/theme-bobthefish jethrokuan/fzf --STDIN
+fisher install oh-my-fish/theme-bobthefish jethrokuan/fzf </dev/null
 cp config.fish $HOME/.config/fish/config.fish
 cp aliases.fish $HOME/.config/fish/aliases.fish
 source $HOME/.config/fish/config.fish
