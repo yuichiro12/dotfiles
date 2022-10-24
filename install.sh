86fb54d96a8a935ad8f13870a6792a6772b0a436
yuichiro12 <talcnrhyme@gmail.com>
2022-10-24 16:57:24 +0900

    source script

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ apt-get install fzf
 fisher install oh-my-fish/theme-bobthefish jethrokuan/fzf
 cp config.fish $HOME/.config/fish/config.fish
 cp aliases.fish $HOME/.config/fish/aliases.fish
+
+source $HOME/.config/fish/config.fish
