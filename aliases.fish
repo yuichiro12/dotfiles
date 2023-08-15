bd6f5612a2813bf19015e669122a0d161c820227
mov <talcnrhyme@gmail.com>
2023-08-15 14:29:49 +0900

    Update aliases.fish

--- aliases.fish
+++ aliases.fish
@@ -2,10 +2,21 @@ alias gta='git add -A'
 alias gtm='git commit -m'
 alias gtp='git push -u'
 alias gtpf='git push --force-with-lease'
-alias la='ls -la'
 alias gb='git branch'
 alias gco='git checkout'
 alias gts='git status'
+alias grv='gh repo view --web'
+alias gpv='gh pr view --web'
+alias gpc='gh pr create'
+
+function z
+	set dir (ghq list | fzf)
+	if test -n "$dir"
+		cd $HOME/workspace/$dir
+	else
+		echo "No directory selected"
+	end
+end
 
 function gtamp
 	git add -A
